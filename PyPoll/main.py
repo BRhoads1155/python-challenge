5194efc391956e41066011f45bdacf6e9ff71b4e
Brendan Rhoads <brendan.rhoads@gmail.com>
2020-08-15 17:51:31 -0400

    JK last commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,22 +5,42 @@ import csv
 #set file path
 pypoll_csv = os.path.join("Resources", "election_data.csv")
 
-#create empty lists 
-total_votes = []
-Khan = []
-Correy = []
-Li = []
-OTooley = []
+#create variables 
+total_votes = 0
+votes_per_candidate = []
+candidates = []
 
 #open csv and read in
 with open(pypoll_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
-#looping through 
+    #looping through 
     for row in csvreader:
-    #track the total number of votes cast
-        total_votes.append(row[0])
-    #track each candidate who recieved votes
-    for i in range(row[2] == "Khan"):
-        Khan.append(len(row[2]))
-        print(Khan)
+        #track the total number of votes cast
+        total_votes +=1
+        #read in the candidate 
+        read_candidate = (row[2])
+        #if candidate already in list then locate the candidate by index # and increment the vote count by 1
+        if read_candidate in candidates:
+            candidate_index = candidates.index(read_candidate)
+            votes_per_candidate[candidate_index] = votes_per_candidate[candidate_index] + 1
+        else:
+            #if candidate was not found in candidates list then append to list and add 1 to vote count
+            candidates.append(read_candidate)
+            votes_per_candidate.append(1)
+
+#print statements
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {len(total_votes)}")
+
+output_path = os.path.join("analysis", "analysis.txt")
+
+with open(output_path, 'w') as txt:
+
+    txt.write(f'Election Results')
+    txt.write(f'-------------------------------')
+    txt.write(f"Total Votes: {total_votes}")
+    txt.write(f"Khan: )
+
+
